1935eb3e0fa82a4af215a87e434abba30c74b05e
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-05-19 13:39:41 +0200

    Fix width on mobile

--- pages/todos.js
+++ pages/todos.js
@@ -14,7 +14,7 @@ const todos = () => {
 
   return (
     <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center mt-32'>
-      <div className='max-w-3xl mx-auto w-4/12'>
+      <div className='max-w-3xl mx-auto lg:w-4/12'>
         <h1 className='text-3xl font-bold '>My todos!</h1>
 
         <form onSubmit={onSubmit}>
